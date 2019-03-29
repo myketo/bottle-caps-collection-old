b12715d60297d9481ece2cd255b234931ce17018
myketo <mykys99@gmail.com>
2019-03-29 18:49:59 +0100

    Autosuggest search results

--- header.php
+++ header.php
@@ -1,23 +1,51 @@
 <?php
     if(isset($_POST['search'])){
         $szukaj = $_POST['szukaj'];
-        $select = $_POST['select'];
+        /*$select = $_POST['select'];
         if($select=='wszystko'){
             header("Location: /szukaj/$szukaj");
         }else{
             header("Location: /szukaj/$select/$szukaj");
-        }
+        }*/
+        header("Location: /szukaj/$szukaj");
     }
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <base href='http://mikolajpiecek.000webhostapp.com/'/>
+        <!--<base href='http://localhost/aaaMOJE/KAPSLE_Kopia/'/>-->
+        <!-- <base href='http://mikolajpiecek.000webhostapp.com'> -->
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="_style/style.css">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
         <title>Kolekcja kapsli</title>
+        <link rel="stylesheet" href="_style/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <script>
+            function searchq(){
+                var searchTxt = $("input[name='szukaj']").val();
+
+                $.post("searchBar.php", {searchVal: searchTxt}, function(output){
+                    $("#output").html(output);
+                });
+
+                $("#output").css('display', 'block');
+            }
+        </script>
+
+        <script>
+            $(document).mouseup(function(e) 
+            {
+                var container = $("#output");
+
+                // if the target of the click isn't the container nor a descendant of the container
+                if (!container.is(e.target) && container.has(e.target).length === 0) 
+                {
+                    container.hide();
+                }
+            });
+        </script>
     </head>
     <body>
 
@@ -26,7 +54,7 @@
                 <div class='header'><a href="/home"><h1>KOLEKCJA KAPSLI</h1></a></div>
                 <div class='search'>
                     <form method="POST">
-                        <select name="select">
+                        <!--<select name="select">
                             <option value="wszystko" <?php if(isset($_GET['select']) && $_GET['select']=='wszystko'){echo "selected='selected'";}; ?>
                             >Wszystko</option>
                             <option value="marka" <?php if(isset($_GET['select']) && $_GET['select']=='marka'){echo "selected='selected'";}; ?>
@@ -37,9 +65,12 @@
                             >Kolor</option>
                             <option value="kraj" <?php if(isset($_GET['select']) && $_GET['select']=='kraj'){echo "selected='selected'";}; ?>
                             >Kraj</option>
-                        </select>
-                        <input type="text" name="szukaj">
-                        <input type="submit" name="search" value="SZUKAJ">
+                        </select>-->
+                        <div class='holder'>
+                            <input type="text" name="szukaj" placeholder='Szukaj...' onkeyup='searchq();' autocomplete='off'>
+                            <div id='output'></div>
+                            <input type="submit" name="search" value="SZUKAJ">
+                        </div>
                     </form>
                 </div>
                 <section class='nav'>
